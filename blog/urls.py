38c6854c3d0bde103f9bf7e74f9bdbd4dbd9e0e0
Emily.Garcia <emilikimberly1@gmail.com>
2021-05-17 18:32:37 -0400

    html templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    #path('register/', user_views.register, name='register'),
+    path('register/', user_views.register, name='register'),
 ]
